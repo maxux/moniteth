b02d70c3f84a19b8fbd11c7eea009085583e22ca
Maxime Daniel <root@maxux.net>
2018-08-04 00:51:42 +0200

    Add protocol header file

---
+++ protocol/moniteth.h
@@ -0,0 +1,29 @@
+#ifndef MONITETH_PROTOCOL_H
+    #define MONITETH_PROTOCOL_H
+
+    typedef enum moth_type_t {
+        MONITETH_TYPE_PING = 0x01,
+        MONITETH_TYPE_PONG = 0x02,
+        MONITETH_TYPE_DS18X20 = 0x03,
+        MONITETH_TYPE_DHT22 = 0x04,
+        MONITETH_TYPE_POWER = 0x05,
+
+    } moniteth_type_t;
+
+    typedef struct moth_ds18_t {
+        uint8_t deviceid[8];
+        int32_t temperature;
+    };
+
+    typedef struct moth_dht22_t {
+        uint16_t deviceid;
+        int32_t temperature;
+        uint8_t humidity;
+    };
+
+    typedef struct moth_power_t {
+        uint8_t phase;
+        int32_t power;
+    };
+
+#endif
